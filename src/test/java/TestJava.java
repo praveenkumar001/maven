f91c3e89a933c14beb6da77f8f309c8d9eee6af7
Praveen <gpraveenkumar001@gmail.com>
2019-08-27 15:59:34 +0530

    HI MAVEN

---
+++ src/test/java/TestJava.java
@@ -0,0 +1,22 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestJava 
+{
+	static
+	{
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	}
+	@Test
+	public void run()
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://www.gmail.com");
+		
+	}
+}
