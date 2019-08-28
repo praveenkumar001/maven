950ccfa693d365161e50dbdd04564dbe9bca7f21
Praveen <gpraveenkumar001@gmail.com>
2019-08-28 12:01:01 +0530

    new

--- src/test/java/TestJava.java
+++ src/test/java/TestJava.java
@@ -1,8 +1,14 @@
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
 public class TestJava 
 {
@@ -11,7 +17,7 @@ public class TestJava
 		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
 	}
 	@Test
-	public void run()
+	public void hi()
 	{
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -19,5 +25,22 @@ public class TestJava
 		driver.get("https://www.gmail.com");
 		System.out.println("hi");
 		
+		ExtentHtmlReporter report=new ExtentHtmlReporter(new File("C:\\Users\\User\\Desktop\\ty\\new.html"));
+		
+		ExtentReports extent=new ExtentReports();
+		
+		ExtentTest test = extent.createTest("praveen");
+		
+		String title=driver.getTitle();
+		
+		if (title.equalsIgnoreCase("gmail"))
+		{
+			test.log(Status.PASS, "title is match");
+		}
+		else
+		{
+			test.log(Status.FAIL, "title is not matched");
+		}
+		extent.flush();
 	}
 }
